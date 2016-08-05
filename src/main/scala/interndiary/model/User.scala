2e0c333f578f34876c3b31044e7291819c10fe89
Asato Wakisaka <asato.wakisaka@gmail.com>
2016-08-05 18:15:22 +0900

    Case

--- src/main/scala/interndiary/model/User.scala
+++ src/main/scala/interndiary/model/User.scala
@@ -2,5 +2,5 @@ package interndiary.model
 
 import org.joda.time.LocalDateTime
 
-case class User(id: Long, name: String, created_at: LocalDateTime, updated_at: LocalDateTime) {
+case class User(id: Long, name: String, createdAt: LocalDateTime, updatedAt: LocalDateTime) {
 }
